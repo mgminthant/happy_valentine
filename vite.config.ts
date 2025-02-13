39f325de8940a9fff498190773824e5c359bd36b
mgminthant <mterror2004@gmail.com>
2025-02-13 18:29:44 +0630

    remove dependencies again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensures Netlify picks up the correct folder
+  },
+  server: {
+    host: true
+  }
+});
